5ae20b850d94138afe9484141c410ece2bdd25f5
Ville Peurala <ville.peurala@gmail.com>
2017-02-05 21:25:55 +0200

    Something

--- src/main/scala/fi/villepeurala/processing/Artwork1.scala
+++ src/main/scala/fi/villepeurala/processing/Artwork1.scala
@@ -3,6 +3,8 @@ package fi.villepeurala.processing
 import processing.core.PApplet
 
 class Artwork1 extends PApplet {
+  var point: (Int, Int) = (0, 0)
+
   override def settings(): Unit = {
     size(1024, 768)
   }
@@ -15,8 +17,18 @@ class Artwork1 extends PApplet {
   override def draw(): Unit = {
     stroke(200)
     strokeWeight(10.0f)
+    val (originX, originY) = point
     if (mousePressed) {
-      line(mouseX, mouseY, pmouseX, pmouseY)
+      line(mouseX, mouseY, originX, originY)
     }
+    if (distance((originX, originY), (mouseX, mouseY)) > 200.0) {
+      point = (mouseX, mouseY)
+    }
+  }
+
+  def distance(p1: (Int, Int), p2: (Int, Int)): Double = {
+    val xDist = p2._1 - p1._1
+    val yDist = p2._2 - p1._2
+    Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
   }
 }
